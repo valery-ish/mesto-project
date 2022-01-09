7d5a3071f5fbf81d312b2739416b698f3905a62c
Rusflipz <Ruslik010101@gmail.com>
2022-01-09 11:25:38 +0300

    refact UserInfo

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,45 +1,54 @@
 import Popup from './Popup.js'
 
 export default class PopupWithForm extends Popup {
-  constructor({ popupSelector, handleSubmit}) {
-      super(popupSelector);
-      this._inputList = this.selector.querySelectorAll('.modal__item');
-      this._modal = this.selector.querySelector('.modal');
-      this._buttonSubmit = this.selector.querySelector('.modal__save-btn');
-      this._handleSubmit = handleSubmit;
-  }
+    constructor({ popupSelector, handleSubmit }) {
+        super(popupSelector);
+        this._popupSelector = popupSelector;
+        this._inputList = this.selector.querySelectorAll('.modal__item');
+        this._modal = this.selector.querySelector('.modal');
+        this._buttonSubmit = this.selector.querySelector('.modal__save-btn');
+        this._handleSubmit = handleSubmit;
+    }
 
-  _getInputValues() {
-    this._formValues = {};
-    this._inputList.forEach(input => this._formValues[input.name] = input.value);
+    _getInputValues() {
+        this._formValues = {};
+        this._inputList.forEach(input => this._formValues[input.name] = input.value);
 
-    return this._formValues;
-  }
+        return this._formValues;
+    }
 
-  _resetModal() {
-    this._modal.reset();
-  }
+    _resetModal() {
+        this._modal.reset();
+    }
 
-  _offAutocomplete() {
-    this._modal.autocomplete = 'off';
-  }
+    _offAutocomplete() {
+        this._modal.autocomplete = 'off';
+    }
 
-  closePopup() {
-    super.closePopup();
-    this._resetModal();
-  }
+    openPopup(data) {
+        super.openPopup();
+        if (this._popupSelector === '.popup_type_profile') {
+            this._inputList[0].value = data.title;
+            this._inputList[1].value = data.description;
+        }
+    }
 
-  handleResultBtnState() {
-    this._buttonSubmit.textContent = 'Сохранено';
-  }
+    closePopup() {
+        super.closePopup();
+        this._resetModal();
+    }
 
-  setEventListeners() {
-      super.setEventListeners();
-      this.selector.addEventListener('submit', (evt) => {
-          evt.preventDefault();
-          this._buttonSubmit.textContent = 'Сохранение...';
-          this._handleSubmit(this._getInputValues())
-      });
-      this._offAutocomplete();
-  }
+    handleResultBtnState() {
+        this._buttonSubmit.textContent = 'Сохранено';
+    }
+
+    setEventListeners() {
+        super.setEventListeners();
+        this.selector.addEventListener('submit', (evt) => {
+            evt.preventDefault();
+            this._buttonSubmit.textContent = 'Сохранение...';
+            this._handleSubmit(this._getInputValues())
+        });
+        this._offAutocomplete();
+    }
 }
