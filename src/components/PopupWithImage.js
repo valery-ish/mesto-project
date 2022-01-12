16c9eeace32c503df02e6578e8335fbeca1f38cb
valery-ish <valery-ish@yandex.ru>
2022-01-12 10:09:05 +0300

    bag

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,8 +3,8 @@ import Popup from './Popup.js'
 export default class PopupWithImage extends Popup {
   constructor(selector, imageModal, imageFigcaption) {
       super(selector);
-      this.currentImageModal = this.selector.querySelector(imageModal);
-      this.currentIimageFigcaption = this.selector.querySelector(imageFigcaption);
+      this.currentImageModal = document.querySelector(imageModal);
+      this.currentIimageFigcaption = document.querySelector(imageFigcaption);
   }
 
   openPopup(title, link) {
